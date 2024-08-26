a5e0c3a1cb44fd5a2f1288b27e7908d1e867bd15
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-08-26 14:30:15 +0300

    display viedo info

--- js/movieInfo.js
+++ js/movieInfo.js
@@ -4,19 +4,24 @@ import {getMovieById ,createMovieList} from "./data/movieData.js";
 // Function to render movies
 function renderMovieInfo(movie) {
   return ` 
-      <div class="movie-info" style="background-image: url('${movie.backdropImage}')">
+      <div class="movie-info" style="background:linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),  url('${movie.backdropImage}');
+        background-size: cover;
+        background-position: center; 
+        background-repeat: no-repeat;" >
+        
             <div class="imgs-container" >
                   <img src="${movie.backdropImage}">
             </div>
 
             <div class="details">
                   <div>Rating : <span>${movie.rating}</span></div>
-                  <div>Time:<span>${movie.runtime}</span></div>
-                  <div>prduction Year:<span>${movie.prodectionDate}</span></div>
-                  <div>price:<span>5$</span></div>
+                  <div>Time : <span>${movie.runtime}</span></div>
+                  <div>prduction Show time : <span>${movie.prodectionDate}</span></div>
+                  <div>price : <span>5$</span></div>
+                  <button class="btn-BookNow">Book Now</button>
             </div>
 
-            <div class ="Info">
+            <div class ="info">
                   <h1 class="film-title">${movie.name}</h1>
                   <p class="description">${movie.story}</p>
             </div>
@@ -31,7 +36,7 @@ async function displayMovieInfo() {
   }
   
   await createMovieList(); // Populate the movieList before using it
-  let movie =getMovieById(533535);
+  let movie =getMovieById(718821);
   console.log(movie);
   container.innerHTML = renderMovieInfo(movie); // Render movie Info
 }
