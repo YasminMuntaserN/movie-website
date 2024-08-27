8797892aa2e9a0f408e6ea4f87cc64b6122fb07a
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-08-27 15:02:50 +0300

    add display Movie Info In Movie Info Box

--- js/BookingPage.js
+++ js/BookingPage.js
@@ -1,7 +1,7 @@
-
+import {getMovieById ,createMovieList} from "./data/movieData.js";
 
 // Function to render movie info in Booking page
-function renderMovieInfo(movie) {
+function renderBookingInfo(movie) {
   return `
     <div class="movie-info">
             <img src="${movie.posterImage}" class="movie-image">
@@ -11,4 +11,31 @@ function renderMovieInfo(movie) {
         </div> 
       `;
   return html;
+}
+
+async function displayMovieInfoInMovieInfoBox() {
+  const container = document.querySelector('.container');
+  if (!container) {
+      return;
+  }
+
+  // Extract movie ID from URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const movieId = urlParams.get('id');
+
+  if (!movieId) {
+      return;
+  }
+
+  // Ensure movieList is populated before using it
+  await createMovieList(); 
+
+  // Get the movie object by ID
+  const movie = getMovieById(movieId);
+  console.log(movieId); 
+  if (movie) {
+      container.innerHTML = renderBookingInfo(movie); // Render movie info
+  } else {
+      console.log(movie); 
+  }
 }
